4379786486b60d434d13cd6ff16f5fc110c75c67
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2022-08-20 15:49:05 +0300

    add: choose order select options

--- FreelanceOrders.py
+++ FreelanceOrders.py
@@ -1,4 +1,6 @@
 import discord
+import discord.interactions
+from discord import SelectMenu, SelectOption
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -13,18 +15,39 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 load_dotenv()
 
-class Menu(discord.ui.View):
+class Order(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=0)
 
     @discord.ui.button(label="Make Order", style=discord.ButtonStyle.green)
     async def menuOrder(self,interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.message.reply('Order in progress')
+        await interaction.message.reply("You are in orders")
+    @discord.ui.select( # the decorator that lets you specify the properties of the select menu
+        placeholder = "Choose an Order Option!",
+        min_values = 1,
+        max_values = 1,
+        options = [
+                discord.SelectOption(
+                    label="Option 1",
+                    description="Pick this if you want [Option 1]!"
+                ),
+                discord.SelectOption(
+                    label="Option 2",
+                    description="Pick this if you want [Option 2]!"
+                ),
+                discord.SelectOption(
+                    label="Option 3",
+                    description="Pick this if you want [Option 3]!"
+                )
+            ]
+    )
+    async def select_Order(self, interaction: discord.Interaction, select: discord.ui.Select):
+        await interaction.response.send_message(f"Nice! You Selected {select.values[0]}")
 
 ### Client-bot commands
 @bot.command()
-async def menu(ctx):
-    view = Menu()
+async def order(ctx):
+    view = Order()
     await ctx.reply(view = view)
 ###
 
