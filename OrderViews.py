a6c6204bf8821b259db3ff44fab17b6df867b08b
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2022-08-26 11:19:50 +0300

    io buffer to discord file

--- OrderViews.py
+++ OrderViews.py
@@ -4,6 +4,7 @@ from Orders import Orders
 from datetime import datetime
 from dotenv import load_dotenv
 import os
+import io
 ### Reference to Orders
 load_dotenv()
 OrdersClass = Orders()
@@ -48,8 +49,9 @@ class modalOrder(discord.ui.Modal, title='Order'):
             timestamp=datetime.now(),
             color=discord.Color.blue())
         embed.set_author(name=interaction.user, icon_url=interaction.user.avatar)
-        path = os.path.abspath(self.AttachedFileName.value)
-        file = discord.File(fp=path, filename='OrderSpec.pdf')
+        with io.open(self.AttachedFileName.value, 'rb') as filePDF:
+            fileBuff = io.BytesIO(filePDF.read())
+        file = discord.File(fp=fileBuff, filename='OrderSpec.pdf')
         await interaction.response.send_message(embed = embed, file=file, view = ViewOrder())
 
 ## Offer Modal
